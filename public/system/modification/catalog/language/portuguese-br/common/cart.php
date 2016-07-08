33cc4d7a002c690520283807a8bd3f9592b3429f
Marco Foganholo <marco@lojavirtual.digital>
2016-07-08 10:54:43 -0300

    translation
    tradução da loja virtual

---
+++ public/system/modification/catalog/language/portuguese-br/common/cart.php
@@ -0,0 +1,11 @@
+<?php
+// Text
+ 
+			$_['text_items2']    = '%s';
+			$_['text_shopping_cart']    = 'Shopping Cart:';
+				
+$_['text_items']     = '%s - %s';
+$_['text_empty']     = 'Seu carrinho está vazio!';
+$_['text_cart']      = 'Exibir carrinho';
+$_['text_checkout']  = 'Finalizar pedido';
+$_['text_recurring'] = 'Assinatura';
---
+++ public/system/modification/catalog/language/portuguese-br/module/bestseller.php
@@ -0,0 +1,21 @@
+<?php
+// Heading
+		
+				$_['text_category'] = 'Categories';
+				$_['text_manufacturer'] = 'Brand:';
+				$_['text_model'] = 'Model:';
+				$_['text_availability'] = 'Availability:';
+				$_['text_instock'] = 'In Stock';
+				$_['text_outstock'] = 'Out Stock';
+				$_['text_price']        = 'Price: ';
+				$_['text_tax']          = 'Ex Tax:';
+				$_['text_quick']          = 'Quick View'; 
+				$_['button_details']          = 'Details';
+				$_['reviews']          = 'reviews ';
+				$_['text_product']          = 'Product {current} of {total} ';
+				$_['button_cart'] = 'Add to cart';
+				
+$_['heading_title'] = 'Mais vendidos';
+
+// Text
+$_['text_tax']      = 'Sem impostos:';
