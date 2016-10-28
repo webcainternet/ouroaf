42ec9f622e3a30b0f16f408b4b1717e54db7e82a
root <root@casrv01.webca.com.br>
2016-10-28 03:41:48 +0000

    server files

--- public/config.php
+++ public/config.php
@@ -6,22 +6,23 @@ define('HTTP_SERVER', 'http://ouroaf.homolog.lojavirtual.digital/');
 define('HTTPS_SERVER', 'http://ouroaf.homolog.lojavirtual.digital/');
 
 // DIR
-define('DIR_APPLICATION', '/Users/fernandomendes/github/ouroaf/public/catalog/');
-define('DIR_SYSTEM', '/Users/fernandomendes/github/ouroaf/public/system/');
-define('DIR_LANGUAGE', '/Users/fernandomendes/github/ouroaf/public/catalog/language/');
-define('DIR_TEMPLATE', '/Users/fernandomendes/github/ouroaf/public/catalog/view/theme/');
-define('DIR_CONFIG', '/Users/fernandomendes/github/ouroaf/public/system/config/');
-define('DIR_IMAGE', '/Users/fernandomendes/github/ouroaf/public/image/');
-define('DIR_CACHE', '/Users/fernandomendes/github/ouroaf/public/system/cache/');
-define('DIR_DOWNLOAD', '/Users/fernandomendes/github/ouroaf/public/system/download/');
-define('DIR_UPLOAD', '/Users/fernandomendes/github/ouroaf/public/system/upload/');
-define('DIR_MODIFICATION', '/Users/fernandomendes/github/ouroaf/public/system/modification/');
-define('DIR_LOGS', '/Users/fernandomendes/github/ouroaf/public/system/logs/');
+define('DIR_APPLICATION', '/var/www/html/public/catalog/');
+define('DIR_SYSTEM', '/var/www/html/public/system/');
+define('DIR_DATABASE', '/var/www/html/public/system/database/');
+define('DIR_LANGUAGE', '/var/www/html/public/catalog/language/');
+define('DIR_TEMPLATE', '/var/www/html/public/catalog/view/theme/');
+define('DIR_CONFIG', '/var/www/html/public/system/config/');
+define('DIR_IMAGE', '/var/www/html/public/image/');
+define('DIR_CACHE', '/var/www/html/public/system/cache/');
+define('DIR_DOWNLOAD', '/var/www/html/public/download/');
+define('DIR_UPLOAD', '/var/www/html/public/system/upload/');
+define('DIR_MODIFICATION', '/var/www/html/public/system/modification/');
+define('DIR_LOGS', '/var/www/html/public/system/logs/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
-define('DB_HOSTNAME', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'root');
+define('DB_HOSTNAME', '172.17.0.1');
+define('DB_USERNAME', 'ouroaf');
+define('DB_PASSWORD', 'nqwhuf7w36d');
 define('DB_DATABASE', 'ouroaf');
 define('DB_PREFIX', 'oc_');
--- public/system/upload/temp-7e066fe49dc831031e4f56b29265c74b/Product Password 1.1.0.1.OC2.x-ocmod/upload/catalog/language/english/module/product_password.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Heading 
-$_['product_password_enter']  = "Please enter password to view this product ";
-$_['product_password_error']  = "<span style='color:#ff0000'>Sorry, your password is not correct.</span>";
-$_['product_password_submit']  = "Submit";
-?>
