09ffd12fbff837145a2158dbb3ee8989408f1ea2
fernandofmendes <fernando.mendes@webca.com.br>
2016-11-07 23:35:29 -0200

    texto

--- public/catalog/language/portuguese-br/payment/pagseguro.php
+++ public/catalog/language/portuguese-br/payment/pagseguro.php
@@ -1,5 +1,5 @@
 <?php
-$_['text_title'] 				= 'PagSeguro - Cartão de Débido e Crédito';
+$_['text_title'] 				= 'PagSeguro - Cartão de Débido e Crédito ';
 $_['button_confirm_pagseguro'] 	= 'Confirmar e ir para o PagSeguro';
 $_['text_extra_amount']			= 'Frete e Taxas';
 ?>
