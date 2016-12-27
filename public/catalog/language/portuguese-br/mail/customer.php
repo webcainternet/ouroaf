60f4ffe3a8337bd530813d97b81af31626001040
fernandofmendes <fernando.mendes@webca.com.br>
2016-12-27 13:16:25 -0200

    email

--- public/catalog/language/portuguese-br/mail/customer.php
+++ public/catalog/language/portuguese-br/mail/customer.php
@@ -2,9 +2,9 @@
 // Text
 $_['text_subject']        = 'Confirmação de cadastro na loja %s.';
 $_['text_welcome']        = 'Obrigado por se cadastrar na loja %s!';
-$_['text_login']          = 'Sua conta foi cadastrada em nossa loja, agora você pode acessá-la utilizando seu e-mail e senha através da nossa loja:';
+$_['text_login']          = 'Seja muito bem-vindo. Seu cadastro e sua compra foram concluídos com perfeição, sua Jóia em breve estará pronta. A Ouro AF se coloca à disposição para qualquer esclarecimento.';
 $_['text_approval']       = 'Sua conta precisa ser aprovada para que você possa acessar os dados de sua conta. Assim que sua conta for aprovada você poderá acessar sua conta utilizando seu e-mail e sua senha através de nossa loja:';
-$_['text_services']       = 'Ao acessar sua conta, você poderá visualizar o histórico de seus pedidos, imprimir faturas, fazer downloads que tenha comprado, modificar as informações de sua conta e muito mais.';
+$_['text_services']       = 'Na Home do site <a href=\"http://ouroaf.com/\" target=\"_blank\">ouroaf.com, tem a área minha conta. Através dela você terá acesso a uma ferramenta administrativa e poderá acompanhara o status do seu pedido, e no caso de boleto bancário, efetuar todos os pagamentos. <a href=\"http://ouroaf.com/index.php?route=account/login\" target=\"_blank\">minha conta</a>. Conheça e curta nossa página do Facebook <a href=\"https://www.facebook.com/ouroafjoalheria\" target=\"_blank\">ouroafjoalheria</a>';
 $_['text_thanks']         = 'Atenciosamente,';
 $_['text_new_customer']   = 'Novo cliente';
 $_['text_signup']         = 'Dados do novo cliente:';
